43c9f2118842f48fe2d08c91b35b2c74525b05d6
TheArkian <54886938+TheArkian@users.noreply.github.com>
2020-06-03 20:09:43 +0100

    Add files via upload

---
+++ Bans.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+// Why are you in this file? There nothing for you to edit
+
+commands = [
+    {
+        name: 'bans',
+        run: async (client, message, args) => {
+            let bans = await message.guild.fetchBans([withReasons = true])
+            let banString = bans
+            .map(ban=> {
+            let string = `${ban.user.tag} was banned for: ${ban.reason ? ban.reason: 'No reason given'}`
+            return string
+            })
+            .join('\n')
+            message.channel.send(banString, { split: true })
+        }
+    }
+]
+
+events = []
+
+
+module.exports = { commands, events }
